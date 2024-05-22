d687b6cbdb0b3652c693ab8f788c6fc403570452
nvinto404 <80949076+nvinto404@users.noreply.github.com>
2024-05-22 22:03:05 +0200

    Änderungen im Server.js File

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hallo das sind die ersten Änderungen und ich hoffe man sieht diese.Ein weiterer Versuch');
+  res.send('Hallo das sind die ersten Änderungen und ich hoffe man sieht diese. Lets go.');
 });
 
 app.listen(PORT, HOST);
